410ce0441c4b9d04fb4d6a76ee7e19df50c6037d
Tim Lai <tuanvu0995@gmail.com>
2024-06-14 19:49:21 +0700

    fix: Happy build
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- packages/graphql/src/scalars/datetime.ts
+++ packages/graphql/src/scalars/datetime.ts
@@ -1,13 +1,17 @@
-import { GraphQLScalarType } from 'graphql'
+// @ts-ignore
 import { DateTime } from 'luxon'
+import { GraphQLScalarType } from 'graphql'
 
 export const DateTimeScalar = new GraphQLScalarType({
   name: 'DateTime',
   description: 'Data type representing the date and time',
-  parseValue: (value: string) => {
+  parseValue: (value: any) => {
     return DateTime.fromISO(value)
   },
-  serialize: (value: DateTime) => {
-    return value.toISO()
+  serialize: (value: any) => {
+    if (value instanceof DateTime) {
+      return value.toISO()
+    }
+    throw Error('GraphQL Date Scalar serializer expected a `Date` object')
   },
 })
--- packages/graphql/tests/graphql.spec.ts
+++ packages/graphql/tests/graphql.spec.ts
@@ -1,4 +1,3 @@
 import { test } from '@japa/runner'
-import { GraphqlCore } from '../src/graphql_core.js'
 
 test.group('GraphqlCore', () => {})
