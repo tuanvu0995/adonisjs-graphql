b737e61abf67d86f1ef627c60669ad710a201105
Tim Lai <tuanvu0995@gmail.com>
2024-06-19 23:23:06 +0700

    fix: Test fail
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- start/env.ts
+++ start/env.ts
@@ -16,15 +16,4 @@ export default await Env.create(new URL('../', import.meta.url), {
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
-
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
 })
