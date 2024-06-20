5899ec7db3053e4f8916853f4bfd3cf1830fe14b
Tim Lai <tuanvu0995@gmail.com>
2024-06-20 12:29:58 +0700

    feat: Belongs to test
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- tests/queries/post.ts
+++ tests/queries/post.ts
@@ -2,11 +2,26 @@ export const CREATE_POST = `
 mutation CreatePost($input: CreatePostInput!) {
   createPost(input: $input) {
     id
-    userId
     title
     content
     createdAt
     updatedAt
+  }
+}
+`
+
+export const GET_POST_WITH_AUTHOR = `
+query GetPost($id: ID!) {
+  post(id: $id) {
+    id
+    title
+    content
+    createdAt
+    updatedAt
+    user {
+      id
+      name
+    }
   }
 }
 `
