91ddec4e822c41e3ccdfdf89093983cba4801144
Tim Lai <tuanvu0995@gmail.com>
2024-06-23 12:12:14 +0700

    fix: Happy lint
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- tests/unit/schema/mieddleware.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@japa/runner'
-import { runMiddlewares } from '../../src/schema/middleware.js'
-
-test.group('Middleware', () => {
-  test('runMiddlewares', async ({ assert }) => {})
-})
